4ae3d8d73c1c56ed87867303d4df2703287eb5f0
NikitaPatil2003 <99522700+NikitaPatil2003@users.noreply.github.com>
2022-09-28 08:42:35 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Demo
+{
+   public static void main(String args[])
+   {
+      int a;
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter the number:- ");
+        a=sc.nextInt();
+
+        System.out.println("The value of a is:- "+a);
+  }
+}

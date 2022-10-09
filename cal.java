40a8ea7a76df2e2bc0f956ac368c9e1a72adaa5b
NikitaPatil2003 <99522700+NikitaPatil2003@users.noreply.github.com>
2022-10-08 23:50:05 -0700

    Add files via upload

---
+++ cal.java
@@ -0,0 +1,22 @@
+class num
+{
+    int a,b,c;  
+    		  
+    void display()
+    {
+    	 a=10;
+         b=20;
+
+    	  c=a+b;
+    	  System.out.print("Addition is:- "+a);
+    }
+}
+public class cal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         num n=new num();
+         n.display();
+	}
+
+}

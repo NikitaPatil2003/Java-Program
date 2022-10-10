b311b4e462ec2fdfa8f67792252994cfb7883786
NikitaPatil2003 <99522700+NikitaPatil2003@users.noreply.github.com>
2022-10-10 10:19:02 -0700

    Add files via upload

---
+++ constructor.java
@@ -0,0 +1,15 @@
+class Demo1
+{
+	 Demo1()
+	{
+		System.out.print("Constructor is called");
+	}
+}
+public class constructor 
+{
+	public static void main(String[] args) 
+	{
+        Demo1 d=new Demo1();
+	}
+
+}
